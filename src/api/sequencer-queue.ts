feabb3e64c19bc06df6ad6ccefe63338362337a5
Tyler van der Hoeven <hi@tyvdh.com>
2024-10-01 13:58:23 -0400

    pass count as arg

--- src/api/sequencer-queue.ts
+++ src/api/sequencer-queue.ts
@@ -9,10 +9,12 @@ export async function apiSequencerQueue(request: RequestLike, env: Env, _ctx: Ex
     if (!await env.SUDOS.get(token))
         return error(401, 'Unauthorized')
 
+    let { count = 1 } = request.query
+
     const sequencerId = env.SEQUENCER_DURABLE_OBJECT.idFromName(SEQUENCER_ID_NAME);
     const sequencerStub = env.SEQUENCER_DURABLE_OBJECT.get(sequencerId) as DurableObjectStub<SequencerDurableObject>;
 
-    await sequencerStub.queueSequences(25)
+    await sequencerStub.queueSequences(count)
 
     return status(204)
 }
