85b0890b2c9478b2c94f4414860f34a3c2fb64f1
Tyler van der Hoeven <hi@tyvdh.com>
2024-10-04 15:06:52 -0400

    clean up simulation errors

--- src/common.ts
+++ src/common.ts
@@ -1,11 +1,6 @@
 import { SorobanRpc, xdr, Keypair, Account, Transaction, FeeBumpTransaction } from "@stellar/stellar-sdk/minimal";
 import { getRpc, wait } from "./helpers";
 
-/* TODO
-    - Add in all the types from stellar-sdk
-        Doesn't look like they're exported so we'll need to manually copy that file over
-*/
-
 export const MAX_U32 = 2 ** 32 - 1
 export const SEQUENCER_ID_NAME = 'Test Launchtube ; June 2024'
 export const EAGER_CREDITS = 100_000
@@ -32,12 +27,23 @@ export async function simulateTransaction(env: Env, tx: Transaction | FeeBumpTra
 
     return rpc.simulateTransaction(tx)
     .then((res) => {
-        if (SorobanRpc.Api.isSimulationRestore(res))
-            throw res // TODO support Restore scenarios
-        else if (SorobanRpc.Api.isSimulationSuccess(res))
+        if (SorobanRpc.Api.isSimulationSuccess(res))
             return res
-        else
-            throw res
+        
+        // TODO support Restore scenarios
+        else if (SorobanRpc.Api.isSimulationRestore(res)) {
+            throw rpc._simulateTransaction(tx)
+        }
+
+        else {
+            const { error, events, ...rest } = res
+
+            throw {
+                error,
+                events: events.map((event) => event.toXDR('base64')),
+                ...rest
+            }
+        }
     })
 }
 
