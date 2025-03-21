276d50c651363b3e1fc9fa4439557bb40c88327b
kalepail <hi@tyvdh.com>
2025-03-21 10:36:47 -0400

    ensure we only send out one notification at a time

--- src/monitor.ts
+++ src/monitor.ts
@@ -6,6 +6,8 @@ const ERROR_INTERVAL = 30; // 30 minutes
 const ERROR_THRESHOLD = 500; // 1000 errors
 
 export class MonitorDurableObject extends DurableObject<Env> {
+    private sending = false;
+
     constructor(ctx: DurableObjectState, env: Env) {
         super(ctx, env);
 
@@ -16,41 +18,54 @@ export class MonitorDurableObject extends DurableObject<Env> {
         })
     }
 
-    async bumpErrorCount() {
-        const errorCount = await this.ctx.storage.get<number>('errorCount') || 0
+    public async bumpErrorCount() {
+        try {
+            const errorCount = await this.ctx.storage.get<number>('errorCount') || 0
 
-        if (errorCount >= ERROR_THRESHOLD) {
-            await this.sendNotification();
-        } else {
-            await this.ctx.storage.put('errorCount', errorCount + 1)
+            if (errorCount >= ERROR_THRESHOLD) {
+                await this.sendNotification();
+            } else {
+                await this.ctx.storage.put('errorCount', errorCount + 1)
+            }
+        } catch (err) {
+            console.error('MONITOR', err);
         }
     }
-    async resetErrorCount() {
+    private async resetErrorCount() {
         await this.ctx.storage.put('errorCount', 0)
     }
-    async sendNotification() {
-        const msg = createMimeMessage();
+    private async sendNotification() {
+        if (this.sending) {
+            return;
+        }
 
-        msg.setSender({ name: "Launchtube Notifications", addr: "noreply@launchtube.xyz" });
-        msg.setRecipient("tyler@stellar.org");
-        msg.setSubject("High Error Count Detected");
-        msg.addMessage({
-            contentType: 'text/plain',
-            data: `
-                Launchtube has detected a high error count. ${ERROR_THRESHOLD} in ${ERROR_INTERVAL} minutes. Please check the logs for more information.
-                https://dash.cloudflare.com/ba55b7ae9acfb3ed152103e3497c0752/workers/services/view/launchtube-prod/production/observability/logs?needle=%7B%22value%22%3A%22%22%2C%22isRegex%22%3Afalse%2C%22matchCase%22%3Afalse%7D&filters=%5B%7B%22key%22%3A%22%24metadata.error%22%2C%22operation%22%3A%22exists%22%2C%22type%22%3A%22string%22%2C%22id%22%3A%22oigq94dh0b%22%7D%5D&view=events&time=%7B%22value%22%3A1%2C%22unit%22%3A%22hours%22%2C%22type%22%3A%22relative%22%7D 
-            `,
-        });
+        try {
+            this.sending = true;
 
-        let message = new EmailMessage(
-            "noreply@launchtube.xyz",
-            "tyler@stellar.org",
-            msg.asRaw()
-        );
-            
-        await this.env.EMAIL.send(message);
+            const msg = createMimeMessage();
 
-        await this.alarm();
+            msg.setSender({ name: "Launchtube Notifications", addr: "noreply@launchtube.xyz" });
+            msg.setRecipient("tyler@stellar.org");
+            msg.setSubject("High Error Count Detected");
+            msg.addMessage({
+                contentType: 'text/plain',
+                data: `
+                    Launchtube has detected a high error count. ${ERROR_THRESHOLD} in ${ERROR_INTERVAL} minutes. Please check the logs for more information.
+                    https://dash.cloudflare.com/ba55b7ae9acfb3ed152103e3497c0752/workers/services/view/launchtube-prod/production/observability/logs?needle=%7B%22value%22%3A%22%22%2C%22isRegex%22%3Afalse%2C%22matchCase%22%3Afalse%7D&filters=%5B%7B%22key%22%3A%22%24metadata.error%22%2C%22operation%22%3A%22exists%22%2C%22type%22%3A%22string%22%2C%22id%22%3A%22oigq94dh0b%22%7D%5D&view=events&time=%7B%22value%22%3A1%2C%22unit%22%3A%22hours%22%2C%22type%22%3A%22relative%22%7D 
+                `,
+            });
+
+            let message = new EmailMessage(
+                "noreply@launchtube.xyz",
+                "tyler@stellar.org",
+                msg.asRaw()
+            );
+                
+            await this.env.EMAIL.send(message);
+            await this.alarm();
+        } finally {
+            this.sending = false;   
+        }
     }
 
     async alarm(info?: AlarmInvocationInfo) {
